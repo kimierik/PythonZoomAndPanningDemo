d9db23e96b0f47be863bcbe01b3c7821d9d33962
kimierik <kimi.malkamaki@gmail.com>
2022-09-03 20:16:56 +0300

    prettify code

--- modules/objectclass.py
+++ modules/objectclass.py
@@ -3,22 +3,6 @@ from . import pgconf
 
 
 
-def matrixmulti(multipmatrix,cordmatrix):
-    #this matrix can only calculate if the following are true
-    #multimatrix will always be x row 3 collum
-    #cord matrix will always be 3row 1 collum
-    #only in cord matrix only input (x,y,z)touple in that format
-    returncords=[0,0,0,0]
-    #x,y,z=cordmatrix
-    iterator=0
-    for mrow in multipmatrix:#mrow is touple and each () in the given matrix
-        for i in range(len(mrow)):#i is every number in the touple
-            returncords[iterator]+=(mrow[i]*cordmatrix[i])  
-            #multiplier always gives us the right answer
-
-        iterator+=1
-
-    return returncords
 
 
 
@@ -55,14 +39,11 @@ class ObjectOwner:
         pygame.draw.rect(self.game.win,self.game.colors["green"],rect)
 
 
-#assumes it is not panned or zoomed
     def world_to_screen(self,cords):
         cords[0]=int((cords[0]-self.panoffset[0])*self.zoomoffset)
         cords[1]=int((cords[1]-self.panoffset[1])*self.zoomoffset)
         return cords
 
-#anything that comes has a direct ref to a screen location
-#as the zoom only exists on screen space we beed to remove it
     def screen_to_world(self,cords):
         cords[0]=(cords[0]/self.zoomoffset)+self.panoffset[0]
         cords[1]=(cords[1]/self.zoomoffset)+self.panoffset[1]
@@ -81,26 +62,9 @@ class ObjectOwner:
 
     def render_G_walls(self):
         for G_wall in self.G_walls:
-            data=G_wall.get_data()
-            data[0]=data[0]*self.XOFFSET#x
-            data[1]=data[1]*self.YOFFSET#y
-            data[2]=data[2]*self.XOFFSET#w
-            data[3]=data[3]*self.YOFFSET#h
-
-            #print(self.screen_to_world([self.game.cursor_x,self.game.cursor_y]))
-            #1-----2
-            #|     |
-            #|     |
-            #4-----3
-
-            #what
-            #place to walls own calss do not call every render like this
-            box_corners=[
-                    [data[0],          data[1]],          
-                    [data[0]+data[2],  data[1]],         
-                    [data[0]+data[2],          data[1]+data[3]], 
-                    [data[0],  data[1]+data[3]],
-                    ]
+            #seperation probably not needed
+            box_corners=[[],[],[],[]]
+            data=G_wall.get_world_corners()
 
             self.panoffset[0]+=(self.cursorDiff[0])/3
             self.panoffset[1]+=(self.cursorDiff[1])/3
@@ -108,9 +72,9 @@ class ObjectOwner:
             #why is cursordiff 3 times too big?
             #tbh idk why it is needed
 
-            for i,item in enumerate(box_corners):
+            for i,item in enumerate(data):
                 box_corners[i]=self.world_to_screen(item)
-                box_corners[i]= item
+                #box_corners[i]= item
 
             pygame.draw.polygon(self.game.win,self.game.colors["black"],box_corners)
 
@@ -128,11 +92,27 @@ class G_wall:
         #w and h is to be changed to how many things in the grid do they take
         #w is how many to the rigth etc
         self.WorldCords=[xc*self.SizeMultipliers[0],yc*self.SizeMultipliers[1],wc*self.SizeMultipliers[0],hc*self.SizeMultipliers[1]]
+        #cannot be assigned on init
+        #bc of how python "compiles" self.corners would be changed even if we did not want it to change
+        """
+        self.corners=(
+                    [self.WorldCords[0],          self.WorldCords[1]],          
+                    [self.WorldCords[0]+self.WorldCords[2],  self.WorldCords[1]],         
+                    [self.WorldCords[0]+self.WorldCords[2],          self.WorldCords[1]+self.WorldCords[3]], 
+                    [self.WorldCords[0],  self.WorldCords[1]+self.WorldCords[3]],
+                    )
+                    """
 
 
     def get_data(self):
         return [self.xc,self.yc,self.wc,self.hc]
 
-
+    def get_world_corners(self):
+        return (
+                    [self.WorldCords[0],          self.WorldCords[1]],          
+                    [self.WorldCords[0]+self.WorldCords[2],  self.WorldCords[1]],         
+                    [self.WorldCords[0]+self.WorldCords[2],          self.WorldCords[1]+self.WorldCords[3]], 
+                    [self.WorldCords[0],  self.WorldCords[1]+self.WorldCords[3]],
+                    )
 
 
